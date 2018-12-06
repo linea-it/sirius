afbee13d82e23eae1fdd44d43a5bee8f8de01bd3
“felipelm” <felipelm123@gmail.com>
2018-12-06 13:27:51 -0200

    api communication

--- src/api.js
+++ src/api.js
@@ -1,7 +1,10 @@
 import Lokka from 'lokka';
 import Transport from 'lokka-transport-http';
 
-const apiUrl = process.env.REACT_APP_API_URL;
+const apiUrl =
+  process.env.NODE_ENV !== 'development'
+    ? window.origin + '/api/graphql'
+    : process.env.REACT_APP_API;
 const client = new Lokka({
   transport: new Transport(apiUrl),
 });
