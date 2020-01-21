0a7a1c00bcc6e49bf6a9830eb2ef9fce98781555
MatheusAllein <mtsallein@gmail.com>
2020-01-21 10:07:02 -0300

    Grouping by class name and adding products' column

--- src/components/userInterfaces/SubtableClasses.js
+++ src/components/userInterfaces/SubtableClasses.js
@@ -3,12 +3,15 @@ import PropTypes from 'prop-types';
 import {
   PagingState,
   IntegratedPaging,
+  GroupingState,
+  IntegratedGrouping,
 } from '@devexpress/dx-react-grid';
 import {
   Grid,
   Table,
   PagingPanel,
   TableHeaderRow,
+  TableGroupRow,
 } from '@devexpress/dx-react-grid-material-ui';
 import moment from 'moment';
 
@@ -30,11 +33,12 @@ export default class SubtableClasses extends Component {
             </span>
           ),
         },
-        { name: 'products', title: 'Products' },
+        { name: 'product', title: 'Product' },
       ],
-      rows: this.props.classesRows,
+      rows: [],
       pageSize: 10,
       pageSizes: [5, 10, 15],
+      defaultExpandedGroups: this.props.classesRows.map(row => row.class),
     };
   }
 
@@ -42,22 +46,46 @@ export default class SubtableClasses extends Component {
     classesRows: PropTypes.array.isRequired,
   };
 
-  // componentDidMount() {
-  //   console.log(this.props)
-  // }
+  componentDidMount() {
+    const rows = [];
+
+    this.props.classesRows.forEach(row =>
+      row.products.forEach(product => {
+        rows.push({
+          class: row.class,
+          name: row.name,
+          version: row.version,
+          versionDate: row.versionDate,
+          product: product,
+        });
+      })
+    );
+
+    this.setState({ rows });
+  }
 
   render() {
-    const { rows, columns, pageSize, pageSizes } = this.state;
-
-    console.log(rows);
+    const {
+      rows,
+      columns,
+      pageSize,
+      pageSizes,
+      defaultExpandedGroups,
+    } = this.state;
 
     return (
       <Grid rows={rows} columns={columns}>
         <PagingState defaultCurrentPage={0} defaultPageSize={pageSize} />
         <IntegratedPaging />
+        <GroupingState
+          grouping={[{ columnName: 'class', showWhenGrouped: true }]}
+          defaultExpandedGroups={defaultExpandedGroups}
+        />
+        <IntegratedGrouping />
         <Table />
         <TableHeaderRow />
         <PagingPanel pageSizes={pageSizes} />
+        <TableGroupRow />
       </Grid>
     );
   }
