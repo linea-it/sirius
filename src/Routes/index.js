5c39f9adaf39833c4c3d1d3d0e970ab87fdd22b1
Fernando Nicolau <fernando_nicolau@outlook.com>
2020-04-29 15:52:28 -0300

    Feat/dataset (#33)
    * chore: dependency router dom.
    
    * feat: title variation per router.
    
    * feat: routes from each separate interface into mains.
    
    * refactor: Files organization.
    
    * fix: Interface name correction.
    
    * Update index.js
    
    * fix: correction of names.
    
    Co-authored-by: Matheus Freitas <mtsallein@gmail.com>

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -4,17 +4,14 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import MainUserInterface from '../components/MainUserInterface';
 import MainDataSet from '../components/MainDataSet';
-// import Home from '../pages/Home';
-// import AboutUs from '../pages/AboutUs';
 
 function Router() {
   return (
     <BrowserRouter>
       <Header />
       <Switch>
-        <Route exact path="/user-interface" component={MainUserInterface} />
-        <Route path="/datasets" component={MainDataSet} />
-        <Route component={MainDataSet} />
+        <Route exact path="/" component={MainUserInterface} />
+        <Route exact path="/datasets" component={MainDataSet} />
       </Switch>
       <Footer />
     </BrowserRouter>
